38cae7119f3590f3d487900f4c48bc6a888d3162
Kevin Akpomuje <32226080+Oberdir@users.noreply.github.com>
2017-09-23 23:26:01 +0200

    # 23.09.2017 -- Kevin Akpomuje

---
+++ input_text.py
@@ -0,0 +1,7 @@
+# 23.09.2017 -- Kevin Akpomuje
+# Ask the user for some input and then displaying the string
+
+# Asking user for input
+print("Please insert any text you like:")
+x = input()
+print("Your input was: ", x)
