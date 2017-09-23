ed2967a8c288498673c2f0b11e39038c04c4d2d7
Kevin Akpomuje <32226080+Oberdir@users.noreply.github.com>
2017-09-23 23:07:55 +0200

    # 23.09.2017 -- Kevin Akpomuje

---
+++ operators.py
@@ -0,0 +1,13 @@
+# 23.09.2017 -- Kevin Akpomuje
+# Operators
+
+# Values
+a = 25
+b = 5
+
+# Calculation
+c = a + b
+
+# Print results on screen
+print("The Challenge: ", a, "+", b)
+print("The Result: ", c)
