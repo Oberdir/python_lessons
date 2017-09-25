f08c60789c837cc3fc355b44057bc0aebc72b78c
Kevin Akpomuje <32226080+Oberdir@users.noreply.github.com>
2017-09-25 20:59:32 +0200

    # 25.09.2017

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# 23.09.2017 -- Kevin Akpomuje
+# Hello World Example
+# Trying to import into PyCharm
+
+print("Hello World!")
---
+++ input_number.py
@@ -0,0 +1,14 @@
+# 25.09.2017 -- Kevin Akpomuje
+
+# Input a Number
+print("Please, input a whole number")
+x = input()
+print("You entered: ", x)
+
+# Convert into a whole number
+xinteger = int(x)
+print("As a whole number:", xinteger)
+
+# Doing a multiplication
+xdouble = xinteger * 2
+print("Double:", xdouble)
